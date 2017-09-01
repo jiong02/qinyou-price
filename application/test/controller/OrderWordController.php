b04cd212c0d56baa21b6d315856802fb1f96f0c3
abc <abc@qq.com>
2017-09-01 11:22:54 +0800

    ok

--- application/test/controller/OrderWordController.php
+++ application/test/controller/OrderWordController.php
@@ -388,7 +388,7 @@ foreach($wordData['customer_data']['key'] as $k=>$v){
     $cell = $table->addCell(2000,$cellStyle);
     $cell->addText($v);
 }
-/*halt($wordData['customer_data']['value']);*/
+
 foreach($wordData['customer_data']['value'] as $k=>$v){
     $table = $section->addTable('myTable');
     $table->addRow();
@@ -420,7 +420,7 @@ foreach($wordData['hotel_data']['key'] as $k=>$v){
     $cell = $table->addCell(3000,$cellStyle);
     $cell->addText($v);
 }
-/*halt($wordData['hotel_data']['value']);*/
+
 foreach($wordData['hotel_data']['value'] as $k=>$v){
     $table = $section->addTable('myTable');
     $table->addRow();
@@ -452,7 +452,7 @@ foreach($wordData['traffic_data']['key'] as $k=>$v){
     $cell = $table->addCell(3000,$cellStyle);
     $cell->addText($v);
 }
-//halt($wordData['traffic_data']['value']);
+
 foreach($wordData['traffic_data']['value'] as $k=>$v){
     $table = $section->addTable('myTable');
     $table->addRow();
